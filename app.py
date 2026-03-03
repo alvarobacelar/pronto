4bc958efaaaa0e7d0320ab61d55e6ea67df1ec0d
Alvaro Bacelar <alvarobsttl@gmail.com>
2026-03-03 15:43:10 -0300

    Ajustado a execução do serviço

--- app.py
+++ app.py
@@ -524,4 +524,6 @@ def delete_escala(id):
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5001)
+    host = os.environ.get("FLASK_RUN_HOST", "127.0.0.1")
+    port = int(os.environ.get("PORT", 5001))
+    app.run(host=host, port=port)
