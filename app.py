6e88ef49404f10347e818d13f6a9749053977208
Alvaro Bacelar <alvarobsttl@gmail.com>
2026-03-08 17:02:09 -0300

    fix: removido o debug true

--- app.py
+++ app.py
@@ -728,4 +728,4 @@ def admin_escala_add():
 if __name__ == "__main__":
     host = os.environ.get("FLASK_RUN_HOST", "127.0.0.1")
     port = int(os.environ.get("PORT", 5001))
-    app.run(host=host, port=port, debug=True)
+    app.run(host=host, port=port)
